e3dfdf3093dc813c996926c62f3221ca1a895d54
Camille <camille.melinjulien@hotmail.com>
2026-01-29 21:53:53 +0100

    Update module.js

--- module.js
+++ module.js
@@ -1,21 +1,35 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase core
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-storage.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDu0_TtOqAybnVLe7Ye1UcUUjbU8513BUA",
-    authDomain: "le-pripri.firebaseapp.com",
-    projectId: "le-pripri",
-    storageBucket: "le-pripri.firebasestorage.app",
-    messagingSenderId: "80244197022",
-    appId: "1:80244197022:web:420e34b41cbcf68f02dd8f",
-    measurementId: "G-BFCPKQKX2Y"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDu0_TtOqAybnVLe7Ye1UcUUjbU8513BUA",
+  authDomain: "le-pripri.firebaseapp.com",
+  projectId: "le-pripri",
+  storageBucket: "le-pripri.firebasestorage.app",
+  messagingSenderId: "80244197022",
+  appId: "1:80244197022:web:420e34b41cbcf68f02dd8f",
+  measurementId: "G-BFCPKQKX2Y"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Init
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// 🔥 exposé globalement
+window.Pripri = {
+  app,
+  analytics,
+  auth,
+  db,
+  storage,
+  user: null
+};
+
+console.log("🔥 Firebase prêt");
