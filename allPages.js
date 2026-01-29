2c89d1d784ba740d160e4410a04df6786023da34
Camille <camille.melinjulien@hotmail.com>
2026-01-29 21:56:42 +0100

    Update allPages.js

--- allPages.js
+++ allPages.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (!window.Pripri) {
+    console.error("Uncaught Error: failled to load Firebase in allPages.js file");
+    return;
+  }
 
+  console.log("Auth dispo :", Pripri.auth);
+});
